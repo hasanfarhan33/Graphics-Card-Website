d47648be7bcdf6cc9cf3e954621a059e7deb17d0
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-08 12:05:49 +0300

    Added a Progress Bar (not working yet)

--- script.js
+++ script.js
@@ -23,22 +23,47 @@ for (let i = 0; i < gameContainers.length; i++) {
     }
 
     if (clickCount === 5) {
-      const downloadButton = document.createElement("button");
+      const generateGraphicsButton = document.createElement("button");
       gamesContainer.style.display = "none";
       chooseGamesText.innerHTML =
-        "Your integrated graphics has been generated!";
-      downloadButton.innerHTML = "Download & Install";
-      downloadButton.style.padding = "10px 20px";
-      downloadButton.style.borderRadius = "20px";
-      downloadButton.style.border = "none";
-      downloadButton.style.backgroundColor = "rgb(40, 86, 128)";
-      downloadButton.style.color = "white";
-      downloadButton.style.marginTop = "5vh";
-      downloadButton.style.marginBottom = "5vh";
-      downloadButton.style.fontSize = "1.5em";
-      downloadButton.style.marginLeft = "42.5vw";
-      downloadButton.style.cursor = "pointer";
-      downloadSection.appendChild(downloadButton);
+        "The games will be added to your integrated graphics";
+      generateGraphicsButton.innerHTML = "Generate & Install Graphics";
+      generateGraphicsButton.style.padding = "10px 20px";
+      generateGraphicsButton.style.borderRadius = "20px";
+      generateGraphicsButton.style.border = "none";
+      generateGraphicsButton.style.backgroundColor = "rgb(40, 86, 128)";
+      generateGraphicsButton.style.color = "white";
+      generateGraphicsButton.style.marginTop = "5vh";
+      generateGraphicsButton.style.marginBottom = "5vh";
+      generateGraphicsButton.style.fontSize = "1.5em";
+      generateGraphicsButton.style.marginLeft = "40vw";
+      generateGraphicsButton.style.cursor = "pointer";
+      downloadSection.appendChild(generateGraphicsButton);
+
+      // DOWNLOAD BUTTON CLICKED HERE
+      generateGraphicsButton.addEventListener("click", function () {
+        generateGraphicsButton.style.display = "none";
+        const progressBarContainer = document.createElement("div");
+        progressBarContainer.style.padding = "10px";
+        progressBarContainer.style.borderRadius = "10px";
+        progressBarContainer.style.width = "70vw";
+        progressBarContainer.style.height = "10vh";
+        progressBarContainer.style.backgroundColor = "#CDDADD";
+        progressBarContainer.style.margin = "10vh 15vw";
+        progressBarContainer.style.display = "relative";
+        progressBarContainer.style.overflow = "hidden";
+        downloadSection.appendChild(progressBarContainer);
+
+        const progressBar = document.createElement("div");
+        var progressBarWidth = 15;
+        progressBar.style.left = "0";
+        progressBar.style.top = "0";
+        progressBar.style.height = "100%";
+        progressBar.style.width = progressBarWidth + "%";
+        progressBar.style.backgroundColor = "rgb(40, 86, 128)";
+        progressBar.style.borderRadius = "10px";
+        progressBarContainer.appendChild(progressBar);
+      });
     }
   });
 }
